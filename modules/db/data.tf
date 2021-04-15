d11028902efdd23cba7b880a92408c416a5aee62
Matias Dell Amerlina <79473194+mdellamerlina@users.noreply.github.com>
2021-04-15 14:19:49 -0700

    Adding DB for development (#11)
    * Adding DB for development
    
    * adding retention as a parameter for db module

--- modules/db/data.tf
+++ modules/db/data.tf
@@ -1,4 +1,8 @@
+locals {
+  environment = (var.env == "production") ? "prod" : "dev"
+}
+
 data "aws_ssm_parameter" "db_password" {
-  name = "/db/${var.db_name}/${var.env}/password"
+  name = "/config/xtages_${var.app}_${local.environment}/spring.datasource.password"
   with_decryption = true
 }
