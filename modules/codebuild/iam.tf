e4dacb98eb6facd79995806315b9b167d05bdc32
Carlos Zuniga <czuniga@xtages.com>
2021-04-29 16:50:04 -0700

    Separates policy definition from role defs

--- modules/codebuild/iam.tf
+++ modules/codebuild/iam.tf
@@ -1,7 +1,6 @@
 resource "aws_iam_role" "xtages_codebuild_ci_role" {
-  name        = "xtages-codebuild-ci-role"
-  description = "Role for the CI builders"
-
+  name               = "xtages-codebuild-ci-role"
+  description        = "Role for the CI builders"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -16,10 +15,12 @@ resource "aws_iam_role" "xtages_codebuild_ci_role" {
   ]
 }
 EOF
+}
 
-  inline_policy {
-    name   = "xtages-codebuild-ci-role-policy"
-    policy = <<EOF
+resource "aws_iam_role_policy" "xtages_codebuild_ci_policy" {
+  name   = "xtages-codebuild-ci-role-policy"
+  role   = aws_iam_role.xtages_codebuild_ci_role.id
+  policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -42,13 +43,11 @@ EOF
   ]
 }
 EOF
-  }
 }
 
 resource "aws_iam_role" "xtages_codebuild_cd_role" {
-  name        = "xtages-codebuild-cd-role"
-  description = "Role for the CD builders"
-
+  name               = "xtages-codebuild-cd-role"
+  description        = "Role for the CD builders"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -63,10 +62,12 @@ resource "aws_iam_role" "xtages_codebuild_cd_role" {
   ]
 }
 EOF
+}
 
-  inline_policy {
-    name   = "xtages-codebuild-cd-role-policy"
-    policy = <<EOF
+resource "aws_iam_role_policy" "xtages_codebuild_cd_policy" {
+  name   = "xtages-codebuild-cd-role-policy"
+  role   = aws_iam_role.xtages_codebuild_cd_role.id
+  policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -99,5 +100,4 @@ EOF
   ]
 }
 EOF
-  }
 }
