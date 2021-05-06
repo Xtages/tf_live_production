4d93ad9188d860e3732022f675a642cbe2ae72d3
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-05-06 11:19:11 -0700

    renaming repo given how ECR manage names

--- ecr/main.tf
+++ ecr/main.tf
@@ -1,10 +1,14 @@
 # ECR repo to store build (CI/CD) Docker images
-resource "aws_ecr_repository" "xtages_build_images" {
-  name                 = "xtages-build-images"
+resource "aws_ecr_repository" "xtages_build_images_node_ci" {
+  name                 = "xtages-build-images/node_ci"
   image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "KMS"
   }
 
   tags = {
