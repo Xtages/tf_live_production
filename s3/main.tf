1b026fb74af4bd5796b9cc30b25917cf5519b2fe
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-05-07 14:22:14 -0700

    adding S3 bucket for buildspec definitions

---
+++ s3/main.tf
@@ -0,0 +1,34 @@
+resource "aws_s3_bucket" "s3_xtages_buildspecs" {
+  bucket = "xtages-buildspecs"
+  acl = "private"
+
+  tags = {
+    Terraform = true
+    Environment = var.env
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_object" "s3_buildspecs_node_ci" {
+  bucket = aws_s3_bucket.s3_xtages_buildspecs.id
+  key = "ci/node/"
+  source = "${path.root}/buildspec-def/ci/node/15.13.0-buildspec.yml"
+}
+
+resource "aws_s3_bucket_object" "s3_buildspecs_node_cd" {
+  bucket = aws_s3_bucket.s3_xtages_buildspecs.id
+  key = "cd/node/"
+  source = "${path.root}/buildspec-def/cd/node/15.13.0-buildspec.yml"
+}
+
