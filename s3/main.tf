acae78f5414920ff194db1ae24295047a0a81eb2
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-05-07 15:37:28 -0700

    adding git commands and rename file

--- s3/main.tf
+++ s3/main.tf
@@ -22,13 +22,13 @@ resource "aws_s3_bucket" "s3_xtages_buildspecs" {
 
 resource "aws_s3_bucket_object" "s3_buildspecs_node_ci" {
   bucket = aws_s3_bucket.s3_xtages_buildspecs.id
-  key = "ci/node/"
-  source = "${path.root}/buildspec-def/ci/node/15.13.0-buildspec.yml"
+  key = "ci/node/15.13.0-buildspec.yml"
+  content = "${path.root}/buildspec-def/ci/node/15.13.0-buildspec.yml"
 }
 
 resource "aws_s3_bucket_object" "s3_buildspecs_node_cd" {
   bucket = aws_s3_bucket.s3_xtages_buildspecs.id
-  key = "cd/node/"
-  source = "${path.root}/buildspec-def/cd/node/15.13.0-buildspec.yml"
+  key = "cd/node/15.13.0-buildspec.yml"
+  content = "${path.root}/buildspec-def/cd/node/15.13.0-buildspec.yml"
 }
 
