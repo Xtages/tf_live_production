b9ca9f7379d04aba09895f143a1f3f7c9b88e991
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-05-10 15:28:27 -0700

    Updating buildspec files to test them

--- s3/main.tf
+++ s3/main.tf
@@ -23,12 +23,12 @@ resource "aws_s3_bucket" "s3_xtages_buildspecs" {
 resource "aws_s3_bucket_object" "s3_buildspecs_node_ci" {
   bucket = aws_s3_bucket.s3_xtages_buildspecs.id
   key = "ci/node/15.13.0-buildspec.yml"
-  content = "${path.root}/buildspec-def/ci/node/15.13.0-buildspec.yml"
+  content_base64 = base64encode( file("${path.root}/buildspec-def/ci/node/15.13.0-buildspec.yml") )
 }
 
 resource "aws_s3_bucket_object" "s3_buildspecs_node_cd" {
   bucket = aws_s3_bucket.s3_xtages_buildspecs.id
   key = "cd/node/15.13.0-buildspec.yml"
-  content = "${path.root}/buildspec-def/cd/node/15.13.0-buildspec.yml"
+  content_base64 = base64encode( file("${path.root}/buildspec-def/cd/node/15.13.0-buildspec.yml") )
 }
 
