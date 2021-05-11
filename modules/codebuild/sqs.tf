01e50561ae6367fcd08b4034276ca4a75c72e605
Carlos Zuniga <czuniga@xtages.com>
2021-05-11 13:27:34 -0700

    Adds a policy to the build_updates_queue (#25)

--- modules/codebuild/sqs.tf
+++ modules/codebuild/sqs.tf
@@ -22,3 +22,27 @@ resource "aws_sqs_queue" "build_updates_queue" {
     Environment = var.env
   }
 }
+
+resource "aws_sqs_queue_policy" "build_updates_queue_policy" {
+  queue_url = aws_sqs_queue.build_updates_queue.id
+  policy    = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "sns.amazonaws.com"
+      },
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.build_updates_queue.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_sns_topic.build_updates_topic.arn}"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
