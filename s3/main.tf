7a817f25419ac2cbc731cadddd96354004000ee1
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-05-20 12:32:28 -0700

    ACM for xtages.dev + S3 bucket for TF

--- s3/main.tf
+++ s3/main.tf
@@ -1,11 +1,14 @@
-resource "aws_s3_bucket" "s3_xtages_buildspecs" {
-  bucket = "xtages-buildspecs"
-  acl    = "private"
-
+locals {
   tags = {
     Terraform   = true
     Environment = var.env
   }
+}
+
+resource "aws_s3_bucket" "s3_xtages_buildspecs" {
+  bucket = "xtages-buildspecs"
+  acl    = "private"
+  tags   = local.tags
 
   versioning {
     enabled = true
@@ -30,4 +33,22 @@ resource "aws_s3_bucket_object" "s3_buildspecs_node_cd" {
   bucket         = aws_s3_bucket.s3_xtages_buildspecs.id
   key            = "cd/node/15.13.0-buildspec.yml"
   content_base64 = base64encode(file("${path.root}/buildspec-def/cd/node/15.13.0-buildspec.yml"))
+}
+
+resource "aws_s3_bucket" "s3_xtages_tf_remote_customers" {
+  bucket = "xtages-tfstate-customers"
+  acl    = "private"
+  tags   = local.tags
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
 }
