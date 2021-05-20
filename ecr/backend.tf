ca10611424f7a7d31fa526857d1c29d3ea14b2e7
Matias Dell Amerlina <79473194+mdellamerlina@users.noreply.github.com>
2021-05-20 10:20:27 -0700

    Infrastructure and buildspec to run CD (#31)
    * Run CD changes
    
    * pulling repos to build and deploy

--- ecr/backend.tf
+++ ecr/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "xtages-tfstate"
-    key    = "tfstate/us-east-1/production/ecr"
-    region = "us-east-1"
+    bucket  = "xtages-tfstate"
+    key     = "tfstate/us-east-1/production/ecr"
+    region  = "us-east-1"
     encrypt = true
   }
 }
--- s3/backend.tf
+++ s3/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "xtages-tfstate"
-    key    = "tfstate/us-east-1/production/s3"
-    region = "us-east-1"
+    bucket  = "xtages-tfstate"
+    key     = "tfstate/us-east-1/production/s3"
+    region  = "us-east-1"
     encrypt = true
   }
 }
