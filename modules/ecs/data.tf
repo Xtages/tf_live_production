375f49b584ad6b68751b02b9483116620794e915
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-06-04 16:18:41 -0700

    enabling ECS task iam role

--- modules/ecs/data.tf
+++ modules/ecs/data.tf
@@ -18,6 +18,8 @@ data "template_file" "ecs_user_data" {
               #!/bin/bash
               echo 'ECS_CLUSTER=xtages-cluster' > /etc/ecs/ecs.config
               echo 'ECS_ENABLE_SPOT_INSTANCE_DRAINING=true' >> /etc/ecs/ecs.config
+              echo 'ECS_ENABLE_TASK_IAM_ROLE=true' >> /etc/ecs/ecs.config
+              iptables-save | tee /etc/sysconfig/iptables && systemctl enable --now iptables
               start ecs
               EOF
 }
