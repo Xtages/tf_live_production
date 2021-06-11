d71564ea186eae7857862baadd26949da494bdb4
Matias Dell Amerlina <79473194+mdellamerlina@users.noreply.github.com>
2021-06-11 10:14:16 -0700

    Removes iptable to make the task role work (#43)

--- modules/ecs/data.tf
+++ modules/ecs/data.tf
@@ -14,12 +14,8 @@ data "aws_ami" "latest_ecs" {
 }
 
 data "template_file" "ecs_user_data" {
-  //  installing iptables to enable IAM roles for tasks
-  // https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-iam-roles.html
   template = <<-EOF
               #!/bin/bash
-              yum install -y iptables-services
-              iptables --insert FORWARD 1 --in-interface docker+ --destination 169.254.169.254/32 --jump DROP
               echo 'ECS_CLUSTER=xtages-cluster' > /etc/ecs/ecs.config
               echo 'ECS_ENABLE_SPOT_INSTANCE_DRAINING=true' >> /etc/ecs/ecs.config
               echo 'ECS_ENABLE_TASK_IAM_ROLE=true' >> /etc/ecs/ecs.config
