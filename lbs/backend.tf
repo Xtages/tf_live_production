cace470ddb645f318375869a271e171fcc85116e
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-07-09 13:15:11 -0700

    adding lb and ecs modules

---
+++ lbs/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "xtages-tfstate"
+    key    = "tfstate/us-east-1/production/lbs/lb-tfstate"
+    region = "us-east-1"
+    encrypt = true
+  }
+}
---
+++ lbs/main.tf
@@ -0,0 +1,30 @@
+data "terraform_remote_state" "xtages_infra" {
+  backend = "s3"
+  config = {
+    bucket = "xtages-tfstate"
+    key    = "tfstate/us-east-1/production"
+    region = "us-east-1"
+  }
+}
+
+module "lb_console" {
+  source            = "git::https://github.com/Xtages/tf_lbs.git?ref=sg_output"
+  alb_name          = "console-lb-${var.env}"
+  env               = var.env
+  aws_region        = var.aws_region
+  vpc_id            = data.terraform_remote_state.xtages_infra.outputs.vpc_id
+  public_subnet_ids = data.terraform_remote_state.xtages_infra.outputs.public_subnets
+  app               = "Console"
+  organization      = "Xtages"
+}
+
+module "lb_customers" {
+  source            = "git::https://github.com/Xtages/tf_lbs.git?ref=sg_output"
+  alb_name          = "customers-lb-${var.env}"
+  env               = var.env
+  aws_region        = var.aws_region
+  vpc_id            = data.terraform_remote_state.xtages_infra.outputs.vpc_id
+  public_subnet_ids = data.terraform_remote_state.xtages_infra.outputs.public_subnets
+  app               = "Customer Apps"
+  organization      = "Xtages"
+}
