b402a7299a7b6cb62cc33189dda902111e806517
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-07-09 15:48:01 -0700

    customers ECS prod cluster

---
+++ ecs-customer-prod/main.tf
@@ -0,0 +1,29 @@
+data "terraform_remote_state" "xtages_infra" {
+  backend = "s3"
+  config = {
+    bucket = "xtages-tfstate"
+    key    = "tfstate/us-east-1/production"
+    region = "us-east-1"
+  }
+}
+
+data "terraform_remote_state" "xtages_lb_infra" {
+  backend = "s3"
+  config = {
+    bucket = "xtages-tfstate"
+    key    = "tfstate/us-east-1/production/lbs/lb-tfstate"
+    region = "us-east-1"
+  }
+}
+
+module "ecs" {
+  source             = "git::https://github.com/Xtages/tf_ecs.git?ref=v0.1.3"
+  cluster_name       = "xtages-customer-production"
+  env                = var.env
+  aws_region         = var.aws_region
+  vpc_id             = data.terraform_remote_state.xtages_infra.outputs.vpc_id
+  private_subnet_ids = data.terraform_remote_state.xtages_infra.outputs.private_subnets
+  public_subnet_ids  = data.terraform_remote_state.xtages_infra.outputs.public_subnets
+  ecs_sg_id          = data.terraform_remote_state.xtages_lb_infra.outputs.customers_ecs_sg_id
+  asg_min_size       = 0
+}
---
+++ ecs-customer-prod/output.tf
@@ -0,0 +1,7 @@
+output "xtages_ecs_cluster_id" {
+  value = module.ecs.xtages_ecs_cluster_id
+}
+
+output "ecs_service_role_arn" {
+  value = module.ecs.ecs_service_role_arn
+}
