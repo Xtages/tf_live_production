852e3282313830e64f2dc5ae2ef126e0a6d81048
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-07-20 12:03:52 -0700

    upgrade ecs module and adding output This also changes the perms for codebuild cd

--- ecs-customer-prod/output.tf
+++ ecs-customer-prod/output.tf
@@ -5,3 +5,7 @@ output "xtages_ecs_cluster_id" {
 output "ecs_service_role_arn" {
   value = module.ecs.ecs_service_role_arn
 }
+
+output "ecs_capacity_provider_name" {
+  value = module.ecs.ecs_capacity_provider_name
+}
