5d65e1ab36a4b75ef02af0ceed9249b1b806b72e
Matias Dell Amerlina <79473194+mdellamerlina@users.noreply.github.com>
2021-08-20 11:22:39 -0700

    Upgrading codebuild module (#63)

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ module "ses" {
 }
 
 module "codebuild" {
-  source     = "git::https://github.com/Xtages/tf_codebuild.git?ref=v0.1.2"
+  source     = "git::https://github.com/Xtages/tf_codebuild.git?ref=v0.1.3"
   env        = var.env
   account_id = var.account_id
 }
