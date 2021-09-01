b5806354261db5a1b060517213ba9c17a7315400
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-08-31 22:03:39 -0700

    upgrading codebuild module version

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ module "db_dev" {
 }
 
 module "codebuild" {
-  source     = "git::https://github.com/Xtages/tf_codebuild.git?ref=v0.1.3"
+  source     = "git::https://github.com/Xtages/tf_codebuild.git?ref=v0.1.4"
   env        = var.env
   account_id = var.account_id
 }
